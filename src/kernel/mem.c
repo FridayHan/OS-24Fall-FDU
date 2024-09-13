4c798ea46c6aa9a9c74c696803f22d4f2aa5d15f
FridayH <hanson2537862904@github.com>
2024-09-13 08:37:35 +0000

    1

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -9,17 +9,23 @@ RefCount kalloc_page_cnt;
 
 void kinit() {
     init_rc(&kalloc_page_cnt);
+// 最好用static限制作用于再当前文件
+    // KERNLINK + end
+    // end & 0xffff
+    // static int page_id;
+
 }
 
 void* kalloc_page() {
     increment_rc(&kalloc_page_cnt);
+// 可用内存从end开始，但是end没有按照4k对齐
 
     return NULL;
 }
 
 void kfree_page(void* p) {
     decrement_rc(&kalloc_page_cnt);
-
+// 不能用递归
     return;
 }
 
