b2f85f2593ab5e638fef4411a216b0c5ab0b87c1
FridayH <hanson2537862904@github.com>
2024-09-26 12:45:24 +0000

    1117

--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -8,27 +8,26 @@
 #include <common/list.h>
 #include <common/string.h>
 
-#define MAX_SIZE_CLASS 10
+#define MAX_SIZE_CLASS 24
 #define USABLE_PAGE_SIZE(block_size) (PAGE_SIZE - round_up((u64)sizeof(Page), (u64)block_size))
+
+static const u16 size_classes[MAX_SIZE_CLASS] = {8, 12, 16, 24, 32, 48, 56, 64, 80, 96, 128, 160, 192, 256, 320, 384, 512, 768, 904, 1024, 1200, 1320, 1720, 2048};
 
 extern char end[];
 
 typedef struct Page {
-    // SpinLock lock;
+    struct Page* prev;
+    struct Page* next;
     int free_list_num;
     u16 block_size;
     u16 free_list_offset;
-    struct Page* next;
+    bool in_free_list;
 } Page;
 
 typedef struct {
     Page* free_pages[MAX_SIZE_CLASS];
     SpinLock locks[MAX_SIZE_CLASS];
 } PagedAllocator;
-
-static const u16 size_classes[MAX_SIZE_CLASS] = {
-    4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048
-};
 
 void kinit_page(Page*, u16);
 u16 align_size(u16);
